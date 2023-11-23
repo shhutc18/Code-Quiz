63fd2cf80cc2b9273cd85fa8361d949b065d0705
Shelby Hutchinson <shhutc18@gmail.com>
2023-11-22 17:01:54 -0800

    added proper comments on index and script files

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Global variables
 let correctDisplay = document.getElementById("correct");
 let wrongDisplay = document.getElementById("wrong");
 let questionDisplay = document.getElementById("question");
@@ -7,6 +8,7 @@ let quiz = document.getElementById("quiz");
 let submitScore = document.getElementById("submitScore");
 let submitScoreBtn = document.getElementById("submitScoreBtn");
 let scoresDisplay = document.getElementById("scores");
+// Array of questions - once the question is answered, it is removed from the array - when the array is empty, the quiz is over
 const questions = [
     {
         question: "Commonly used data types DO NOT include:",
@@ -38,9 +40,13 @@ let question;
 let timer;
 let timeLeft = 75; 
 
+// Event listener is in the index page which calls this function when the start button is clicked
 function startQuiz() {
+    // Removes the start button and displays the quiz
     document.getElementById("startButton").style.display = "none";
+    // Displays the questions and answers
     document.querySelector(".answers").style.display = "block";
+    // Starts the timer
     timeDisplay.textContent = "Time:" + timeLeft;
     timer = setInterval(function () {
         timeLeft--;
@@ -49,13 +55,17 @@ function startQuiz() {
             showScore();
         }
     }, 1000);
+    // Displays the first question
     displayQuestion();
 }
 
 function displayQuestion() {
+    // Selects a random question from the questions array
     question = questions[Math.floor(Math.random() * questions.length)];
+    // Displays the question
     questionDisplay.textContent = question.question;
 
+    // Itterates through the possible answers to the question
     for (let i = 0; i < question.answers.length; i++) {
         let btn = document.getElementById("btn" + (i + 1));
         btn.textContent = question.answers[i];
@@ -63,12 +73,16 @@ function displayQuestion() {
     }
 }
 
+// Checks if the user clicked on the correct answer
 function checkAnswer() {
+    // Hide the correct and wrong display
     correctDisplay.style.display = "none";
     wrongDisplay.style.display = "none";
+     // Checks if the user clicked on the correct answer
     if (question.correct === this.textContent) {
         correctDisplay.style.display = "block";
     } else {
+        // if not, display the wrong display and subtract 10 seconds from the timer
         wrongDisplay.style.display = "block";
         timeLeft -= 10; 
     }
@@ -82,22 +96,34 @@ function checkAnswer() {
     displayQuestion();
 }
 
+// Ends the quiz and displays the user's score and a form to submit their score
 function showScore() {
+    // Stops the timer
     clearInterval(timer);
+    // Adds the user's score to the dom
     document.querySelector("#userScore").innerHTML = "Your final score is " + timeLeft;
+    // Shows the submit score section
     submitScore.style.display = "flex";
+    // Hides the quiz section
     quiz.style.display = "none";
 }
 
+// Shows the leader board section and sorts the scores from highest to lowest
 function showLeaderBoard() {
+    // Stops the timer incase the user clicks on the view high scores button while taking the quiz
     clearInterval(timer);
+    // Hides the quiz and submit score sections
     quiz.style.display = "none";
     submitScore.style.display = "none";
+    // Shows the leaderboard section
     leaderBoard.style.display = "flex";
+    // Gets the scores from local storage
     let scores = JSON.parse(localStorage.getItem("scores"));
+    // Sorts the scores from highest to lowest
     scores.sort(function (a, b) {
         return b.score - a.score;
     });
+    // Resets the scores display
     scoresDisplay.innerHTML = "";
     for (let score of scores) {
         let li = document.createElement("li");
@@ -106,26 +132,40 @@ function showLeaderBoard() {
     }
 }
 
+// called by event listener - refreshes the page which starts the quiz over
 function startScreen () {
     window.location.reload();
 }
 
+// called by event listener - erases the leader board from local storage and resets the scores display
 function resetLeaderBoard() {
     localStorage.removeItem("scores");
     scoresDisplay.innerHTML = "";
 }
 
+// event listener is called when the user clicks on the submit score button after the quiz is over
 submitScoreBtn.addEventListener("click", function (event) {
+    // prevents the page from refreshing
     event.preventDefault();
     let score = {
         score: timeLeft,
         initials: document.getElementById("initials").value
     }
+    // checks if the user entered their initials
+    if (score.initials === "") {
+        alert("Please enter your initials");
+        return;
+    }
+    // gets the scores from local storage
     let scores = JSON.parse(localStorage.getItem("scores"));
+    // creates an empty array if there are no scores in local storage
     if (scores === null) {
         scores = [];
     }
+    // adds the new score to the array
     scores.push(score);
+    // saves the array to local storage
     localStorage.setItem("scores", JSON.stringify(scores));
+    // displays the leader board
     showLeaderBoard();
 });
